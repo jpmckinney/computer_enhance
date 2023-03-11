e6dbaf519ba0096fa3f0313fdf43caffe044f24b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-03-11 02:20:33 -0500

    feat: Support a bunch more instructions (todo: call, jmp, ret, int, segments)

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ const REG_NAMES: [[&str; 8]; 2] = [
     ["al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"],
     ["ax", "cx", "dx", "bx", "sp", "bp", "si", "di"],
 ];
-
 const R_M_NAMES: [&str; 8] = ["bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx"];
-
-const OP_NAMES: [&str; 8] = ["add", "or", "adc", "sbb", "and", "sub", "xor", "cmp"];
-
+const SEGMENT_NAMES: [&str; 4] = ["es", "cs", "ss", "ds"];
+const UNARY2_NAMES: [&str; 4] = ["inc", "dec", "push", "pop"];
+const UNARY3_NAMES: [&str; 8] = ["test", "xxx", "not", "neg", "mul", "imul", "div", "idiv"];
+const BINARY_NAMES: [&str; 8] = ["add", "or", "adc", "sbb", "and", "sub", "xor", "cmp"];
+const LOGIC_NAMES: [&str; 8] = ["rol", "ror", "rcl", "rcr", "shl", "shr", "xxx", "sar"];
 const JUMP4_NAMES: [&str; 16] = [
     "jo", "jno", "jb", "jnb", "je", "jne", "jbe", "jnbe", "js", "jns", "jp", "jnp", "jl", "jnl", "jle", "jnle",
 ];
-
 const JUMP2_NAMES: [&str; 4] = ["loopnz", "loopz", "loop", "jcxz"];
 
 fn next_i16(iterator: &mut Enumerate<Bytes<BufReader<File>>>, w: bool) -> i16 {
@@ -26,14 +26,6 @@ fn next_i16(iterator: &mut Enumerate<Bytes<BufReader<File>>>, w: bool) -> i16 {
         i16::from_le_bytes([byte, iterator.next().unwrap().1.unwrap()])
     } else {
         i16::from(i8::from_le_bytes([byte]))
-    }
-}
-
-const fn operation(byte: u8, mov: bool) -> &'static str {
-    if mov {
-        "mov"
-    } else {
-        OP_NAMES[((byte >> 3) & 0b111) as usize]
     }
 }
 
@@ -76,9 +68,7 @@ fn run(filename: &str) -> Vec<String> {
         // println!("{byte1:8b}");
         // continue;
 
-        // Register/memory to/from register.
-        // 100010 D W MOV
-        // 00###0 D W ADD, etc.
+        // +reg +r/m +disp -data ("Register/memory to/from register.").
         //
         // mov bp, [1024]
         // mov al, [bx + si]
@@ -86,10 +76,28 @@ fn run(filename: &str) -> Vec<String> {
         // mov ax, [bx + di - 8]
         // mov dx, [bp]
         // mov si, bx
-        if byte1 >> 2 == 0b100010 || (byte1 >> 2) & 0b110001 == 0 {
-            let mov = byte1 >> 7 == 1;
-            let d = (byte1 >> 1) & 1 == 1;
-            let w = (byte1 & 1) as usize;
+        //
+        // 100010 D W MOV
+        if byte1 >> 2 == 0b100010
+            // 00...0 D W ADD, etc.
+            || (byte1 >> 2) & 0b110001 == 0
+            // 100001 0 W TEST
+            || (byte1 >> 1) == 0b1000010
+            // 100001 1 W XCHG
+            || byte1 >> 1 == 0b1000011
+            // LEA
+            || byte1 == 0b10001101
+            // LDS
+            || byte1 == 0b11000101
+            // LES
+            || byte1 == 0b11000100
+        {
+            // LEA, LDS and LES use REG for the destination and are wide.
+            let (d, w) = if byte1 == 0b10001101 || byte1 == 0b11000101 || byte1 == 0b11000100 {
+                (1, 1)
+            } else {
+                ((byte1 >> 1) & 1, (byte1 & 1) as usize)
+            };
 
             // MOD REG R/M
             let byte2 = iterator.next().unwrap().1.unwrap();
@@ -97,25 +105,30 @@ fn run(filename: &str) -> Vec<String> {
             let reg = ((byte2 >> 3) & 0b111) as usize;
             let r_m = (byte2 & 0b111) as usize;
 
-            let op_text = operation(byte1, mov);
+            let op_text = match byte1 {
+                0b10001101 => "lea",
+                0b11000101 => "lds",
+                0b11000100 => "les",
+                _ => match byte1 >> 1 {
+                    0b1000010 => "test",
+                    0b1000011 => "xchg",
+                    0b1000100 | 0b1000101 => "mov",
+                    _ => BINARY_NAMES[((byte1 >> 3) & 0b111) as usize],
+                },
+            };
+
             let reg_text = REG_NAMES[w][reg];
             let r_m_text = disassemble_r_m(&mut iterator, w, m0d, r_m);
 
             // 1 = the REG field identifies the destination operand.
             // 0 = the REG field identifies the source operand.
-            if d {
-                instructions.insert(position, format!("{op_text} {reg_text}, {r_m_text}"));
+            if d == 1 {
+                instructions.insert(position, format!("{op_text} {reg_text}, {r_m_text}\n"));
             } else {
-                instructions.insert(position, format!("{op_text} {r_m_text}, {reg_text}"));
+                instructions.insert(position, format!("{op_text} {r_m_text}, {reg_text}\n"));
             }
 
-        // Immediate to register/memory.
-        // 110001 1 W MOV
-        // 100000 S W ADD, etc.
-        // 100000 0 W AND
-        // 100000 0 W OR
-        //
-        // Nothing is 100000 1 X, so it's OK to treat AND and OR as having an S bit.
+        // -reg +r/m +disp ?data ("Immediate to register/memory." "Register/memory.")
         //
         // mov [1024], byte 8
         // mov [bx + si], byte 8
@@ -123,27 +136,68 @@ fn run(filename: &str) -> Vec<String> {
         // mov [bx + di - 8], word 2048
         // mov [bp], byte 8
         // mov bx, word 2048
-        } else if byte1 >> 1 == 0b1100011 || byte1 >> 2 == 0b100000 {
-            let mov = byte1 >> 6 == 0b11;
-            let s = (byte1 >> 1) & 1;
+        //
+        // TEST "Immediate data and register/memory" has the same first byte as NEG, etc.
+        // so we need to put them all in this branch - but the latter do not have DATA bytes.
+        //
+        // 110001 1 W MOV
+        } else if byte1 >> 1 == 0b1100011
+            // 100000 S W ADD, etc.
+            // 100000 0 W AND
+            // 100000 0 W OR
+            //
+            // It's OK to treat AND and OR as having an S bit.
+            || byte1 >> 2 == 0b100000
+            // The following do not have DATA bytes.
+            //
+            // 100011 1 1 POP
+            || byte1 == 0b10001111
+            // 111111 1 1 PUSH
+            || byte1 == 0b11111111
+            // 111111 1 W INC, DEC
+            || byte1 >> 1 == 0b1111111
+            // 111101 1 W NEG, MUL, IMUL, DIV, IDIV, NOT, TEST
+            || byte1 >> 1 == 0b1111011
+            // 110100 V W SHL SHR SAR ROL ROR RCL RCR
+            || byte1 >> 2 == 0b110100
+        {
+            let op = byte1 >> 2;
+            let mov = op == 0b110001;
+            let s_v = (byte1 >> 1) & 1;
             let w = (byte1 & 1) as usize;
 
-            // MOD ### R/M
+            // MOD ... R/M
             let byte2 = iterator.next().unwrap().1.unwrap();
             let m0d = byte2 >> 6; // mod
             let r_m = (byte2 & 0b111) as usize;
 
             let r_m_text = disassemble_r_m(&mut iterator, w, m0d, r_m);
 
-            // data | data if w = 1 or data | data if sw = 01
-            let data = next_i16(&mut iterator, (mov || s == 0) && w == 1);
-
-            let op_text = operation(byte2, mov);
+            let op_text = match op {
+                0b110001 => "mov",
+                0b100011 => "pop",
+                0b100000 => BINARY_NAMES[((byte2 >> 3) & 0b111) as usize],
+                0b111111 => UNARY2_NAMES[((byte2 >> 3) & 0b11) as usize],
+                0b111101 => UNARY3_NAMES[((byte2 >> 3) & 0b111) as usize],
+                0b110100 => LOGIC_NAMES[((byte2 >> 3) & 0b111) as usize],
+                _ => unreachable!(),
+            };
             let unit = if w == 1 { "word" } else { "byte" };
 
-            instructions.insert(position, format!("{op_text} {r_m_text}, {unit} {data}"));
+            // Binary operators (MOV, TEST, ADD, etc.) have DATA bytes.
+            if op == 0b110001 || op == 0b100000 || op == 0b111101 && (byte2 >> 3).trailing_zeros() >= 3 {
+                // data | data if w = 1 or data | data if sw = 01
+                let data = next_i16(&mut iterator, (mov || s_v == 0) && w == 1);
+                instructions.insert(position, format!("{op_text} {r_m_text}, {unit} {data}\n"));
+            } else if op == 0b110100 {
+                // 0 = Shift/rotate count is one. 1 = Shift/rotate count is specified in CL register.
+                let count = if s_v == 0 { "1" } else { "cl" };
+                instructions.insert(position, format!("{op_text} {unit} {r_m_text}, {count}\n"));
+            } else {
+                instructions.insert(position, format!("{op_text} {unit} {r_m_text}\n"));
+            }
 
-        // MOV Immediate to register.
+        // +reg +data ("MOV Immediate to register.")
         // 1011 W REG
         //
         // mov cl, 8
@@ -157,32 +211,104 @@ fn run(filename: &str) -> Vec<String> {
 
             let reg_text = REG_NAMES[w][reg];
 
-            instructions.insert(position, format!("mov {reg_text}, {data}"));
+            instructions.insert(position, format!("mov {reg_text}, {data}\n"));
 
-        // Memory to accumulator. Accumulator to memory. Immediate to accumulator.
-        // 101000 E W MOV
-        // 00###1 0 W ADD, etc.
+        // +data ("Memory to accumulator." "Accumulator to memory." "Immediate to accumulator.")
         //
         // mov ax, [8]
         // mov ax, [1024]
-        } else if byte1 >> 2 == 0b101000 || (byte1 >> 1) & 0b1100011 == 0b10 {
-            let mov = byte1 >> 7 == 1;
+        // 101000 E W MOV
+        } else if byte1 >> 2 == 0b101000
+            // 00...1 0 W ADD, etc.
+            || (byte1 >> 1) & 0b1100011 == 0b10
+            // 101010 0 W TEST
+            || (byte1 >> 1) == 0b1010100
+            // 111001 E W IN, OUT
+            || (byte1 >> 2) == 0b111001
+        {
+            let mov = byte1 >> 2 == 0b101000;
+            let port = (byte1 >> 2) == 0b111001;
             let e = (byte1 >> 1) & 1 == 0; // opposite of d
             let w = byte1 & 1 == 1;
 
-            // addr-lo | addr-hi or data | data if w = 1
-            let addr = next_i16(&mut iterator, mov || w);
+            // addr-lo | addr-hi or data | data if w = 1 or data-8
+            let addr = if port {
+                i16::from(u8::from_le_bytes([iterator.next().unwrap().1.unwrap()]))
+            } else {
+                next_i16(&mut iterator, mov || w)
+            };
 
-            let op_text = operation(byte1, mov);
+            let op_text = match byte1 >> 1 {
+                0b1010000 | 0b1010001 => "mov",
+                0b1010100 => "test",
+                0b1110010 => "in",
+                0b1110011 => "out",
+                _ => BINARY_NAMES[((byte1 >> 3) & 0b111) as usize],
+            };
+            let reg_text = if w { "ax" } else { "al" };
+            let addr_text = if mov { format!("[{addr}]") } else { addr.to_string() };
+
+            if e {
+                instructions.insert(position, format!("{op_text} {reg_text}, {addr_text}\n"));
+            } else {
+                instructions.insert(position, format!("{op_text} {addr_text}, {reg_text}\n"));
+            }
+
+        // PUSH POP INC DEC Register. One byte: 010 .. REG
+        } else if (byte1 >> 5) == 0b010 {
+            let reg = (byte1 & 0b111) as usize;
+
+            let reg_text = REG_NAMES[1][reg];
+            let op_text = UNARY2_NAMES[((byte1 >> 3) & 0b11) as usize];
+
+            instructions.insert(position, format!("{op_text} {reg_text}\n"));
+
+        // PUSH POP Segment register. One byte: 000 SG 11 .
+        } else if (byte1 & 0b11100111) == 0b110 || (byte1 & 0b11100111) == 0b111 {
+            let sg = ((byte1 >> 3) & 0b11) as usize;
+
+            let sg_text = SEGMENT_NAMES[sg];
+            let op_text = if (byte1 & 1) == 0 { "push" } else { "pop" };
+
+            instructions.insert(position, format!("{op_text} {sg_text}\n"));
+
+        // XCHG Register with accumulator. One byte: 10010 REG
+        } else if (byte1 >> 3) == 0b10010 {
+            let reg = (byte1 & 0b111) as usize;
+
+            let reg_text = REG_NAMES[1][reg];
+
+            instructions.insert(position, format!("xchg ax, {reg_text}\n"));
+
+        // IN OUT Variable port. One byte: 111011 . W
+        } else if (byte1 >> 2) == 0b111011 {
+            let out = (byte1 >> 1) & 1 == 1;
+            let w = byte1 & 1 == 1;
+
             let reg_text = if w { "ax" } else { "al" };
 
-            if !mov {
-                instructions.insert(position, format!("{op_text} {reg_text}, {addr}"));
-            } else if e {
-                instructions.insert(position, format!("{op_text} {reg_text}, [{addr}]"));
+            if out {
+                instructions.insert(position, format!("out dx, {reg_text}\n"));
             } else {
-                instructions.insert(position, format!("{op_text} [{addr}], {reg_text}"));
+                instructions.insert(position, format!("in {reg_text}, dx\n"));
             }
+
+        // REP.
+        } else if byte1 == 0b11110011 {
+            let byte2 = iterator.next().unwrap().1.unwrap();
+            let w = byte2 & 1 == 1;
+
+            let op_text = match byte2 >> 1 {
+                0b1010010 => "movs",
+                0b1010011 => "cmps",
+                0b1010111 => "scas",
+                0b1010110 => "lods",
+                0b1010101 => "stos",
+                _ => unreachable!(),
+            };
+            let unit = if w { "b" } else { "w" };
+
+            instructions.insert(position, format!("rep {op_text}{unit}\n"));
 
         // 0111   JUMP
         // 111000 JUMP
@@ -201,11 +327,54 @@ fn run(filename: &str) -> Vec<String> {
             let length = labels.len();
             let label = labels.entry(target).or_insert_with(|| format!("label{length}"));
 
-            instructions.insert(position, format!("{op_text} {label} ; {ip_inc8}"));
+            instructions.insert(position, format!("{op_text} {label} ; {ip_inc8}\n"));
 
-        // Debugging.
+        // Two fixed bytes.
+        } else if byte1 == 0b11010100 || byte1 == 0b11010101 {
+            let byte2 = iterator.next().unwrap().1.unwrap();
+
+            let op_text = if byte1 & 1 == 0 { "aam" } else { "aad" };
+
+            if byte2 == 0b00001010 {
+                instructions.insert(position, format!("{op_text}\n"));
+            } else {
+                unreachable!();
+            }
+
+        // One fixed byte.
         } else {
-            instructions.insert(position, format!("{byte1:8b}"));
+            let op_text = match byte1 {
+                0b11010111 => "xlat\n",
+                0b10011111 => "lahf\n",
+                0b10011110 => "sahf\n",
+                0b10011100 => "pushf\n",
+                0b10011101 => "popf\n",
+                0b00110111 => "aaa\n",
+                0b00100111 => "daa\n",
+                0b00111111 => "aas\n",
+                0b00101111 => "das\n",
+                0b10011000 => "cbw\n",
+                0b10011001 => "cwd\n",
+                0b11001110 => "into\n",
+                0b11001111 => "iret\n",
+                0b11111000 => "clc\n",
+                0b11110101 => "cmc\n",
+                0b11111001 => "stc\n",
+                0b11111100 => "cld\n",
+                0b11111101 => "std\n",
+                0b11111010 => "cli\n",
+                0b11111011 => "sti\n",
+                0b11110100 => "hlt\n",
+                0b10011011 => "wait\n",
+                0b11110000 => "lock ",
+                _ => "",
+            };
+            if op_text.is_empty() {
+                // Debugging.
+                instructions.insert(position, format!("{byte1:8b}\n"));
+            } else {
+                instructions.insert(position, op_text.to_string());
+            }
         };
     }
 
@@ -224,7 +393,7 @@ fn main() {
     let filename = env::args().nth(1).unwrap();
     let mut stdout = io::stdout().lock();
     for line in run(&filename) {
-        writeln!(stdout, "{line}").unwrap();
+        write!(stdout, "{line}").unwrap();
     }
 }
 
@@ -244,7 +413,7 @@ mod tests {
         let mut assembly_file = File::create(assembly_path.clone()).unwrap();
 
         for line in run(test_path) {
-            writeln!(assembly_file, "{line}").unwrap();
+            write!(assembly_file, "{line}").unwrap();
         }
 
         let mut text = vec![];
